bae81316e501a91900d37fa6de45d5cf75c24593
Alberto Becerra Tomé <alberto.becerra.tome@gmail.com>
2021-01-31 13:16:01 +0000

    Apache Benchmark

--- Exercise_starter_files/benchmark.sh
+++ Exercise_starter_files/benchmark.sh
@@ -70,4 +70,4 @@
 # Total:        114  160  28.0    172     199#
 
 
- ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer REPLACE_WITH_KEY' http://REPLACE_WITH_API_URL/score
+ ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer nkQb6U9P2jMlvLk3w1aDWcc8479GmXmH' http://e1883366-558b-4b48-aa36-55d69308884d.southcentralus.azurecontainer.io/score
