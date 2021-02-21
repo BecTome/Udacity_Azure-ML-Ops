7b30abbd28789ed2dc9d71d635b3268e1b0471bf
Alberto Becerra Tomé <alberto.becerra.tome@gmail.com>
2021-02-21 20:12:21 +0000

    Delete trial file

--- starter_files/endpoint_handmad.py
+++
@@ -1,9 +0,0 @@
-import request
-
-uri = ''
-key = ''
-
-data = [{'age':[57], 'job':['technician'], 'marital':['married'], 
-	'education':['high.school'], ....}]
-header = {'auth':key}
-req = request.post(uri, data, header=header)
